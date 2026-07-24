4f462b26f7e3bec1a585839eae034b8668864071
deepart09 <deeparts09@gmail.com>
2026-07-24 11:39:41 +0530

    new

---
+++ minheap.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class minheap {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(30);
+        pq.add(10);
+        pq.add(20);
+        pq.add(5);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+    }
+}
